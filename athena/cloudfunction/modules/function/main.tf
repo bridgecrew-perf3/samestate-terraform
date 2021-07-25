89fce89c85d18ffcefb9a14e106f30a7f9b16c4a
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2021-07-25 16:42:21 +0530

    Update main.tf

--- athena/cloudfunction/modules/function/main.tf
+++ athena/cloudfunction/modules/function/main.tf
@@ -6,6 +6,6 @@ resource "google_cloudfunctions_function" "function" {
   available_memory_mb   = var.function_memory
   source_archive_bucket = var.archive_bucket
   source_archive_object = var.archive_object
-  trigger_http          = 256
+  trigger_http          = 128
   entry_point           = "hello_world"
 }
