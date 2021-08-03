60bb2ecd8adfc86dba9cfa3737ee4f0e7b4b5241
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2021-08-03 22:09:11 +0530

    Update main.tf

--- sitecore/pubsub/main.tf
+++ sitecore/pubsub/main.tf
@@ -9,4 +9,13 @@ module "pubsub" {
     topic_name = var.topic_name
     topic_labels = var.topic_labels
     allowed_persistence_regions = var.allowed_persistence_regions
+    
+    subscription_name = var.subscription_name
+    message_retention_duration = var.message_retention_duration
+    retain_acked_messages = var.retain_acked_messages
+    expiration_policy_ttl = var.expiration_policy_ttl
+    retry_policy_minimum_backoff = var.retry_policy_minimum_backoff
+    retry_policy_maximum_backoff = var.retry_policy_maximum_backoff
+    enable_message_ordering = var.enable_message_ordering
+
 }
