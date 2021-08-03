0637ba5407aed0c4e4673ee9e8559b27a780e21b
Shriyut Jha <46967628+Shriyut@users.noreply.github.com>
2021-08-04 00:47:19 +0530

    Update main.tf

--- sitecore/storage/modules/folder/main.tf
+++ sitecore/storage/modules/folder/main.tf
@@ -34,26 +34,26 @@ resource "google_storage_bucket_object" "archive_folder" {
   name = var.archive_folder_name
   content = var.archive_folder_content
   bucket = var.bucketname
-  depend_on = [google_storage_bucket.bucket]
+  
 }
 
 resource "google_storage_bucket_object" "log_folder" {
     name = var.log_folder_name
     content = var.log_folder_content
     bucket = var.bucketname
-    depend_on = [google_storage_bucket.bucket]
+    
 }
 
 resource "google_storage_bucket_object" "temp_folder" {
     name = var.temp_folder_name
     content = var.temp_folder_content
     bucket = var.bucketname
-    depend_on = [google_storage_bucket.bucket]
+    
 }
 
 resource "google_storage_bucket_object" "staging_folder" {
     name = var.staging_folder_name
     content = var.staging_folder_content
     bucket = var.bucketname
-    depend_on = [google_storage_bucket.bucket]
+   
 }
